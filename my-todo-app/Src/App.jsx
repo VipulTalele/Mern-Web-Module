4635f865150b9cc9c8324386b7fef2f950c10c97
VipulTalele <vipultalele14792@gmail.com>
2025-11-23 15:44:05 +0530

    Create App.jsx

---
+++ my-todo-app/Src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ToDoComponent from "./ToDoComponent";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <ToDoComponent />
+      </div>
+    );
+  }
+}
+
+export default App;
